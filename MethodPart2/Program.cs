805f54be10ea3a8b1e344e41836831a4189457b5
ParvizHajili <parviz.hajili@gmail.com>
2024-03-25 18:54:27 +0400

    Methods

---
+++ MethodPart2/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+
+namespace MethodPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Mail adresini ayırıb ekrana yazdırmaq
+            //Mən info@codelandia.edu.az adresindən mail aldım
+            // Cümləsindən mail adresini ayırıb ekrana yazdırmaq.
+            //string text = Console.ReadLine();
+            //string[] words = text.Split(' ');
+            //foreach (var item in words)
+            //{
+            //    /*
+            //     for (int i = 0; i < item.Length; i++) 
+            //     {
+            //         if (item[i] == '@')
+            //         {
+            //             Console.WriteLine(item);
+            //         }
+            //     }*/
+
+            //    if (item.Contains("@"))
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion Mail adresini ayırıb ekrana yazdırmaq
+
+            #region verilmiş sözün bütün hərflərini böyüdün. 
+            //verilmiş sözün ToUpper metodu istifadə etmədən
+            //bütün hərflərini böyüdün. 
+            #region version 1
+            /*char[] lowerCase = { 'a', 'b', 'c' };
+            char[] upperCase = { 'A', 'B', 'C' };
+
+            string text = "abC";
+            string newText = "";
+            bool isRepeat = false;
+            foreach (var simvol in text)
+            {
+                for (int i = 0; i < lowerCase.Length; i++)
+                {
+                    if (lowerCase[i] == simvol)
+                    {
+                        newText += upperCase[i];
+                        isRepeat = true;
+                        break;
+                    }
+
+                }
+                if (!isRepeat)
+                {
+                    newText += simvol;
+                }
+                isRepeat = false;
+            }
+            Console.WriteLine(newText);*/
+            #endregion version 1
+
+            #region version 2
+            //string text = Console.ReadLine();
+            //string newText = "";
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (text[i] >= 'a' && text[i] <= 'z')
+            //    {
+            //        newText += (char)(text[i] - 32);
+            //    }
+            //    else
+            //    {
+            //        newText += text[i];
+            //    }
+            //}
+            //Console.WriteLine(newText);
+            #endregion version 2
+
+            #endregion verilmiş sözün  bütün hərflərini böyüdün. 
+
+            #region Verilmiş massivdən təkrarlanan elementləri silin.  
+            //Verilmiş massivdən təkrarlanan elementləri silin.  
+            //int[] arr = { 1, 2, 3, 5, 6, 8, 3, 99, 45, 22, 1, 108, 99 };
+            //ArrayList newArr = new ArrayList();
+            //bool isRepeat = false;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    //start loop
+            //    foreach (int item in newArr)
+            //    {
+            //        if (arr[i] == item)
+            //        {
+            //            isRepeat = true;
+            //            break;
+            //        }
+            //    }
+            //    if (!isRepeat)
+            //    {
+            //        newArr.Add(arr[i]);
+            //    }
+            //    isRepeat = false;
+
+            //    //end loop
+            //}
+
+            //foreach (var item in newArr)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            #endregion Verilmiş massivdən təkrarlanan elementləri silin.  
+
+            #region Login and password
+            // Statik olaraq username və parol saxlayın.
+            // Və istifadəçidən bir username parol istəyin
+            // əgər username və parol eyni olarsa "Sistemə girişiniz uğurludur" yazılsın.
+            // Əgər 3 dəfə yanlış cəhd edərsə "Sistemə girişiniz bloklandı" yazılsın.
+            string dataLogin = "admin";
+            string dataPassword = "admin123";
+            string login, password;
+            int count = 3;
+
+            while (count > 0)
+            {
+                count--;
+                Console.Write("Login: ");
+                login = Console.ReadLine();
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+                if (login == dataLogin && password == dataPassword)
+                {
+                    Console.WriteLine("Success!");
+                    break;
+                }
+                else
+                {
+                    if (count == 0)
+                    {
+                        Console.WriteLine("Blocked!");
+                        break;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("{0} cəhdiniz qaldı!",count);
+                }
+            }
+            
+            #endregion Login and password
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,233 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region StringBuilder
+            //string name = "Parviz ";
+            //string surName = "Hajili";
+
+            //string fullName = name + surName;
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.Append(name);
+            //sb.Append(surName);
+            //sb.Append(1);
+            //sb.Append(2);
+            //sb.Append(3);
+            //sb.Append(4);
+            //sb.Append(5);
+
+
+
+            //Console.WriteLine(sb);
+            #endregion
+
+            #region Methods
+            /*
+             accessmodifier returnType MethodName ()
+
+            {
+            }
+             
+             */
+
+
+            /*
+             Şəkil yüklənmə funksionallığı
+             */
+
+
+            /*
+             Şəkil yüklənmə funksionallığı
+             */
+
+
+            /*
+             ImageUpload()
+             */
+
+
+            /*
+             1) Geriyə dəyər qaytarmır və parametr qəbul etmir
+             2) Geriye dəyər qaytarmır parametr qəbul edir
+             3) Geriyə dəyər qaytarır parametr qəbul etmir
+             4) Geriyə dəyər qaytarır parametr qəbul edir
+             */
+            #endregion
+
+            #region MyRegion
+            //Test2();
+
+            //int a = Sum(5, 10);
+            //Console.WriteLine(a);
+
+            //int result = Sum(10, 100);
+            //int result = Sum2(b: 10, c: 20, a: 30);
+
+            //Console.WriteLine(result);
+            //Console.WriteLine(Sum2(b: 10, c: 20, a: 30));
+            #endregion
+
+            #region Params
+            //int a
+            //int result = Sum(2, 4, 6, 7, 21, 32, 43, 54);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Overloading
+
+            //string name = "Codelandia";
+
+            //Sum(10, 20, 20);
+            #endregion
+        }
+        #region 4 method variation
+        //public void Method1()
+        //{
+
+        //}
+
+        //public void Method2(string s)
+        //{
+
+        //}
+
+        //public int Method3()
+        //{
+        //    return 373478;
+        //}
+
+        //public int Method4(int s)
+        //{
+        //    return 0;
+        //}
+        //public void MethodLocalFunctionExample()
+        //{
+        //    Sum(5);
+
+        //    /*
+        //     some code
+
+        //     */
+        //    int Sum(int x)
+        //    {
+        //        return 5;
+        //    }
+        //}
+        #endregion
+
+        #region MyRegion
+        //public static void Test2()
+        //{
+        //    Console.WriteLine("World");
+
+        //    WriteToConsole();
+
+        //    void WriteToConsole()
+        //    {
+        //        Console.WriteLine("Hello ");
+        //        Console.WriteLine("Hello ");
+        //        Console.WriteLine("Hello ");
+        //        Console.WriteLine("Hello ");
+        //        Console.WriteLine("Hello ");
+        //    }
+        //}
+        //public static void Test(in int b)
+        //{
+        //    // b dəyişkənin dəyərini dəyişmək olmaz
+        //    //b = b + 5;
+        //    Console.WriteLine(b);
+        //}
+
+        //public static int Sum1(int a,int b)
+        //{
+        //    int c = a + b;
+        //    return c;
+        //    Console.WriteLine(c);
+        //}
+
+        //public static int Sum(int c, int a, int b = 5)
+        //{
+        //    return a + b + c;
+        //}
+
+
+        //public static int Sum2(int a, int b, int c)
+        //{
+        //    return c + b + a;
+        //}
+
+
+        #endregion
+
+        #region Params
+        //public static int Sum(params int[] values)
+        //{
+        //    int sum = 1;
+
+        //    foreach (int value in values)
+        //    {
+        //        sum *= value;
+        //    }
+
+        //    return sum;
+        //}
+
+        //public static int Sum4(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //public static int Sum1(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+        //public static int Sum2(int a, int b, int c, int d)
+        //{
+        //    return a + b + c + d;
+        //}
+
+        #endregion
+
+        #region Overloading
+        /// <summary>
+        /// Bu metod gonderilen 2 ededin cemini toplamaq ucundur
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        //public static int Sum(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+        /// <summary>
+        ///  Bu metod gonderilen 3 ededin cemini toplamaq ucundur
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        //public static int Sum(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+        //public static int Sum(byte a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+        //public static decimal Sum(decimal a, int b, int c, sbyte d)
+        //{
+        //    return a + b + c + d;
+        //}
+
+        #endregion
+    }
+
+}
