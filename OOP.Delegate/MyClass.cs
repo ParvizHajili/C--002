a5c03090c8acd38691882065a687315b312f21b9
ParvizHajili <parviz.hajili@gmail.com>
2024-04-15 16:42:12 +0400

    delegates

---
+++ OOP.Delegate/MyClass.cs
@@ -0,0 +1,75 @@
+﻿namespace OOP.Delegate
+{
+    public class MyClass
+    {
+        Action myActionDelegate;
+        Action<int, int, string, Student> myAction;
+
+        Func<int, string, bool> myFunc;
+
+        Predicate<int> Predicate;
+
+        public static List<Student> Students = new List<Student>();
+
+        public int MyProperty { get; set; }
+
+        public delegate bool MyValidationDelegate(Student student);
+
+        static MyValidationDelegate MyNameValidation = FirstNameIsNull;
+        static MyValidationDelegate MyNameLengthValidation = FirstNameMinLength;
+        static MyValidationDelegate AgeCannotNegativeValidation = IsAgeNegative;
+
+
+        public static void MyCustomAddMethod(Student student)
+        {
+            bool result = ValidationHandler(student,
+                MyNameValidation,
+                MyNameLengthValidation,
+                AgeCannotNegativeValidation);
+
+            if (result)
+            {
+                Students.Add(student);
+            }
+            else
+            {
+                Console.WriteLine("Məlumatları düzgün daxil edin");
+            }
+
+            return;
+        }
+
+
+        public static bool FirstNameIsNull(Student student)
+        {
+            return !string.IsNullOrEmpty(student.FirstName);
+        }
+
+        public static bool FirstNameMinLength(Student student)
+        {
+            return student.FirstName.Length > 2;
+        }
+
+        public static bool IsAgeNegative(Student student)
+        {
+            return student.Age > 0;
+        }
+
+        public static bool ValidationHandler(Student student, params MyValidationDelegate[] validationDelegates)
+        {
+            foreach (var validation in validationDelegates)
+            {
+                if (!validation(student))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public sbyte Age { get; set; }
+    }
+}
---
+++ OOP.Delegate/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace OOP.Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyClass myClass = new MyClass(); 
+            //MyClass.MyValidationDelegate
+            #region Delegate
+            //Console.WriteLine("Hello, World!");
+
+            //XDelegate xDelegate = new XDelegate(ConsoleWriteFive);
+
+            //Sum(4, xDelegate);
+            //xDelegate += () =>
+            //{
+            //    Console.WriteLine("Test1");
+            //};
+
+            //xDelegate += () => { WriteName(); };
+
+
+            //xDelegate += Test2;
+
+            //xDelegate.Invoke();
+
+            //xDelegate -= Test2;
+
+            //xDelegate += () => { Test3(); };
+            //xDelegate += () => { Test2(); };
+
+            ////xDelegate += Sum(3);
+            //xDelegate.Invoke();
+            #endregion
+
+            Student student = new()
+            {
+                FirstName = "Kanan",
+                LastName = "Axundov",
+                Age = -17
+            };
+
+            MyClass.MyCustomAddMethod(student);
+
+            Console.WriteLine(MyClass.Students.Count);
+
+            Console.ReadLine();
+        }
+
+        public delegate void XDelegate();
+
+        public static void ConsoleWriteFive()
+        {
+            Console.WriteLine(5);
+        }
+
+        public static void WriteName()
+        {
+            Console.WriteLine("Name");
+        }
+
+        public static void Test2()
+        {
+            Console.WriteLine("Test2");
+        }
+
+        public static void Test3()
+        {
+            Console.WriteLine("Test3");
+        }
+
+        public static int Sum(int a, XDelegate xDelegate)
+        {
+            if (a == 5)
+            {
+                xDelegate.Invoke();
+                //return 1;
+            }
+            return 1;
+        }
+    }
+}
